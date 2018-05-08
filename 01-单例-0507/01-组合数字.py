4e4c538d156745ed16c39deef30d2d6db1a8ea01
lws820 <963755497@qq.com>
2018-05-08 22:20:31 +0800

    .

--- 01-单例-0507/01-组合数字.py
+++
@@ -1,7 +0,0 @@
-# coding=utf-8
-
-for i in range(1, 5):
-    for j in range(1, 5):
-        for k in range(1, 5):
-            if (i != j) and (i != k) and (j !=k):
-                print(i,j,k)
--- 02-获取ip-0508/gethostIPDemo2.py
+++
@@ -1,7 +0,0 @@
-import socket
-
-hostname = socket.gethostname()
-ip = socket.gethostbyname(hostname)
-# ip = socket.gethostbyname_ex(hostname)
-
-print (ip)
