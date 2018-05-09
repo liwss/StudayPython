012c1553f72147723d2caea887ac4910d417c951
lws820 <963755497@qq.com>
2018-05-09 23:17:51 +0800

    add Process study

--- day0509-多进程/fork.py
+++ day0509-多进程/fork.py
@@ -5,6 +5,6 @@ import os
 ret = os.fork()
 
 if ret == 0:
-	print "我是子进程：%d ,我的父进程是:%d" %(os.getpid(), os.getppid())
+    print "我是子进程：%d , 我的父进程是: %d"% (os.getpid(), os.getppid())
 else:
-	print "我是父进程：%d" %(os.getpid())    
+    print "我是父进程：%d"% (os.getpid())
---
+++ day0509-多进程/process.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+# @Author : lws
+# @Time   : 2018/5/9 22:24
+
+
+from multiprocessing import Process
+import time
+
+
+def test(name):
+    for i in range(3):
+        print "hello %s" % name
+        time.sleep(1)
+
+if __name__ == "__main__":
+    p = Process(target=test, args=("liws",))
+    p.start()
+    p.join(1)      # 是否等待进程实例结束，参数为等待timeout
+    print "_____end_____"
+
+
+
