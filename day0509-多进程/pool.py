00a3543c042cac12332fc94c4930ae1d34ab4efd
lws820 <963755497@qq.com>
2018-05-10 16:44:42 +0800

    add pool apply

--- day0509-多进程/pool.py
+++ day0509-多进程/pool.py
@@ -10,13 +10,14 @@ from multiprocessing import Pool
 
 def worker(msg):
     print "%d begin，processing：%d" % (msg, os.getpid())
-    time.sleep(2)
+    time.sleep(1)
 
 
 if __name__ == "__main__":
     p = Pool(3)
     for i in range(10):
-        p.apply_async(worker, (i,))
+        # p.apply_async(worker, (i,))  # apply_async非阻塞式，并行执行，
+        p.apply(worker, (i,)) # apply(func[, args[, kwds]])：使用阻塞方式调用func
 
     print "____start____%d" % os.getpid()
     p.close()
