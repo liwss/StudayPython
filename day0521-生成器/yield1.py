412ee64ed7385a4e94ef1897db3293c8d694ad86
lws820 <963755497@qq.com>
2018-05-21 22:46:06 +0800

    生成器的创建

---
+++ day0521-生成器/yield1.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+# @Author   : lws
+# @Time     : 2018/5/21 22:35
+# @describe : 创建生成器
+
+a = [x for x in range(10)]
+b = iter(a)
+
+# print next(b)   # 打印生成器值
+# print next(b)
+# print next(b)
+
+
+# for i in b:       # 遍历生成器
+#     print i
+
+
+
+
+
+
+
+
