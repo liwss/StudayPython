c71471bacc0ef7b61688776e2d4fad93fdfcfdbe
lws820 <963755497@qq.com>
2018-05-21 22:55:11 +0800

    yield next()

---
+++ day0521-生成器/yield2.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+# @Author   : lws
+# @Time     : 2018/5/21 22:46
+# @describe : yield创建生成器
+
+
+def Test():
+    i = 0
+    while True:
+        yield i   # 每次执行都会卡在yeild处
+        i+=1
+
+
+t = Test()
+print t.next()
+print t.next()
+print t.next()
+
+
+
+
+
+
+
+
+
+
