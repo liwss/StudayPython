0f96cade434c222ed3037fda60ef360dd5b8f051
lws820 <963755497@qq.com>
2018-08-28 17:52:08 +0800

    logging

--- day0807-logging/demo1.py
+++ day0807-logging/demo1.py
@@ -6,4 +6,17 @@
 
 import logging
 
+LOG_FORMAT = "%(asctime)s %(levelname)s |>>>>> [%(message)s] "  # 配置输出日志格式
+DATE_FORMAT = '%Y-%m-%d  %H:%M:%S '  # 配置输出时间的格式，注意月份和天数不要搞乱了
+logging.basicConfig(level=logging.DEBUG,
+                    format=LOG_FORMAT,
+                    datefmt=DATE_FORMAT,
+                    filename=r"D:\code\python\StudayPython\day0807-logging\debug.log"
+                    # 有了filename参数就不会直接输出显示到控制台，而是直接写入文件
+                    )
+rows = '10'
+logging.debug("msg1:"+rows)
+logging.debug("msg1")
+logging.debug("msg1")
+
 
