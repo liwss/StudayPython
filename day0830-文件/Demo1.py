efd535a7452a7044b9d6a2b98979401fd853df58
lws820 <963755497@qq.com>
2018-09-03 15:11:55 +0800

    文件操作

--- day0830-文件/Demo1.py
+++ day0830-文件/Demo1.py
@@ -12,12 +12,19 @@ import time
 #     print (index, '-----------'+data.strip())
 #     time.sleep(0.5)
 
-f = open("test.txt", "r")   # 可以读取大文件且效率高，f迭代器方式取出，每读取一行就会覆盖之前的，所以不会内存不足
-for line in f:
-    print (line.strip())
+# f = open("test.txt", "r")   # 可以读取大文件且效率高，f迭代器方式取出，每读取一行就会覆盖之前的，所以不会内存不足
+# for line in f:
+#     time.sleep(1)
+#     print (line.strip())
+#     # print (f.encoding)   # 打印文件编码
+#     print (f.name)         # 打印文件名称
+# f.close()                  # 关闭文件句柄，使用with open() as:可以不用手动关闭句柄
 
-
-
+with open("test.txt", "r") as f:
+    a = f.readlines()
+    b = f.readline()
+    print (a)
+    print (b)
 
 
 
