b2de02988727269cd3796a111f85d146783576ce
lws820 <963755497@qq.com>
2018-11-30 14:49:41 +0800

    tornado异步http请求，回调方式

---
+++ day0823-Tornado/http_request_1.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+# @Author   : lws
+# @Time     : 2018/11/27 17:12
+# @describe : tornado.gen模块执行异步http请求demo
+
+import tornado.web
+import tornado.options
+import tornado.httpserver
+import tornado.ioloop
+
+
+from tornado.options import define, options
+
+define("port", default=8000, type=int, help="run on geiven port")
+
+
+class IndexHandler(tornado.web.RequestHandler):
+    pass
+
+if __name__ == '__main__':
+    tornado.options.parse_command_line()
+    app = tornado.web.Application(handlers=[(r'/lws/test', IndexHandler)])
+    http_server = tornado.httpserver.HTTPServer(app)
+    http_server.listen(options.port)
+    tornado.ioloop.IOLoop.instance().start()
